91da57a7fe4d5c84a3b19a733670357036dc8398
Zulfahmi Ardiansah <zulfahmiardiansah.26@gmail.com>
2024-10-16 21:10:43 +0700

    feat: delete all unneeded migration and change email verify

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,9 +13,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('emp')->unique();
             $table->string('name');
+            $table->string('position')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('address')->nullable();
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
