2c63cd1c798b77abedaf13b5ac8a3ffc6eca052f
Zulfahmi Ardiansah <zulfahmiardiansah.26@gmail.com>
2025-01-13 19:56:52 +0700

    fix: fixing status bug

--- resources/views/request/include/main.blade.php
+++ resources/views/request/include/main.blade.php
@@ -78,7 +78,7 @@
                         <path d="M12 7a5 5 0 1 1 -4.995 5.217l-.005 -.217l.005 -.217a5 5 0 0 1 4.995 -4.783z" />
                     </svg>
                     Cuti Disetujui
-                @elseif ($application->status == 99)
+                @elseif ($application->status == 0)
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor" class="icon text-danger">
                         <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                         <path d="M12 7a5 5 0 1 1 -4.995 5.217l-.005 -.217l.005 -.217a5 5 0 0 1 4.995 -4.783z" />
